60add9f69a62b92e207d1a92c97a4d09aac90731
João Victor Mendes <mendesjoaovic@gmail.com>
2019-10-16 17:00:19 -0300

    Ambientes 1 à 4 modelados

--- Trabalho 1/projeto_amb3.pl
+++ Trabalho 1/projeto_amb3.pl
@@ -1,28 +1,30 @@
-% Teste do ambiente 1: solucao_bl([[1, 1, 0],[[2, 3]],[[9, 1], [10, 5]]], S).
+% Teste do ambiente 3: solucao_bl([[1, 1, 0],[[6, 3], [1, 4]],[[10, 1], [2, 5], [8, 5], [10, 5]]], S).
 
 % Definindo tamanho do prédio
 % tam_predio([10, 5]).
 dentro_predio([X, Y|_]) :- X > 0, Y > 0, X < 11, Y < 6.
 
 % Mapeando paredes no ambiente
-ocupado_com([3, 3], parede).
-ocupado_com([7, 5], parede).
+ocupado_com([6, 1], parede).
+ocupado_com([5, 3], parede).
 
 % Mapeando entulhos no ambiente
-ocupado_com([4, 2], entulho).
+ocupado_com([3, 1], entulho).
+ocupado_com([3, 2], entulho).
+ocupado_com([6, 2], entulho).
 ocupado_com([7, 2], entulho).
-ocupado_com([4, 4], entulho).
-ocupado_com([7, 4], entulho).
+ocupado_com([6, 4], entulho).
 ocupado_com([6, 5], entulho).
 
 % Mapeando escadas no ambiente
 % Acho q é possível mapear somente uma escada, para subir, e fazer a verificação de que, se houver uma escada em [X, Y - 1], pode descer.
 ocupado_com([5, 1], escada).
-ocupado_com([9, 2], escada).
-ocupado_com([1, 3], escada).
+ocupado_com([9, 1], escada).
+ocupado_com([1, 2], escada).
+ocupado_com([8, 2], escada).
+ocupado_com([4, 3], escada).
 ocupado_com([10,3], escada).
 ocupado_com([3, 4], escada).
-ocupado_com([5, 4], escada).
 ocupado_com([9, 4], escada).
 
 % Verificando se existe fogo ou extintor
